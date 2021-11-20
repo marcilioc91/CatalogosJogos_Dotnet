dd2b9ff3fd4bc21da4d64ef61712a7ae12fcf3c8
marcilioc91 <marcilioccsilva@fac.pe.senac.br>
2021-11-19 22:19:18 -0300

    Subindo projeto

---
+++ ApiCatalogoJogos/ApiCatalogoJogos/Services/IJogoService.cs
@@ -0,0 +1,21 @@
+﻿using ApiCatalogoJogos.InputModel;
+using ApiCatalogoJogos.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Services
+{
+    public interface IJogoService : IDisposable
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+        Task<JogoViewModel> Obter(Guid id);
+        Task<JogoViewModel> Inserir(JogoViewModel jogo);
+        Task Atualizar(Guid id, JogoViewModel jogo);
+        Task Atualizar(Guid id, double preco);
+        Task Remover(Guid id);
+        Task Inserir(JogoInputModel jogoInputModel);
+        Task Atualizar(Guid idJogo, JogoInputModel jogoInputModel);
+    }
+}
